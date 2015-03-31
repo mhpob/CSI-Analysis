065e4e598536aebdabc3d697e0d9a2f13005a14a
mhpob <mikeob9@gmail.com>
2015-03-31 13:42:19 -0400

    Fix overplotting of legends.

--- active_ggvis.R
+++ active_ggvis.R
@@ -1,14 +1,18 @@
-library(rgdal); library(ggplot2); library(ggvis); library(dplyr)
+library(rgdal); library(ggvis); library(dplyr)
 
 ## Load shapefiles. Shapefiles downloaded from USGS.
-york <- readOGR('p:/obrien/gis/shapefiles', 'YKPKRivs')
+york <- readOGR('p:/obrien/gis/shapefiles', 'YKPKCLIP')
 
-york@data$ID <- ifelse(york@data$FTYPE %in% c(364, 460), 'pk', 'yk')
+york <- york[york$PERMANENT_ %in%
+            c("120007612", "120007614", "128576612", "143959607"),]
+
+york@data$ID <- ifelse(york$PERMANENT_ %in% c(128576612, 143959607), 'yk',
+                       ifelse(york$PERMANENT_ == 120007614, 'pk', 'mp'))
 york <- spChFIDs(york, paste0(york@data$ID, row.names(york)))
 
 ## fortify() to turn the object into a data frame suitable for 
 # plotting with ggplot2
-yk.df <- fortify(york)
+yk.df <- ggplot2::fortify(york)
 
 ## filter() out the points I don't want to have plotted.
 yk.plot <- filter(yk.df, grepl('yk', group))
@@ -18,15 +22,21 @@ pk.plot <- filter(yk.df, grepl('pk', group))
 pass.dat <- read.csv('p:/obrien/biotelemetry/csi/listening/activedata.csv',
                      header = T, stringsAsFactors = F)
 pass.dat$Detections <- as.factor(pass.dat$Detections)
+pass.dat <- pass.dat %>% mutate(pred.growth = TelemetryR::sturgrow(Temp, Sal, DO.pct),
+         river = substr(Site.ID, 1, 2))
 yk.dat <- filter(pass.dat, grepl('YK', Site.ID))
 
 
-ggvis(yk.dat, x = ~DD.Long, y = ~DD.Lat) %>%
-  filter(Cruise == eval(input_select(choices = unique(yk.dat$Cruise)))) %>%
-  layer_points(fill = ~Sal,
+
+ggvis(pass.dat, x = ~DD.Long, y = ~DD.Lat) %>%
+  filter(Cruise == eval(input_select(choices = unique(pass.dat$Cruise)))) %>%
+  layer_points(fill = ~pred.growth,
                size := 150) %>%
   scale_numeric('fill', range=c('blue','pink')) %>%
   layer_points(size = ~Detections) %>%
   scale_ordinal('size', domain = c(0, 1)) %>%
-  layer_paths(x = ~long, y = ~lat, data = group_by(yk.plot, group))
-  
+  layer_paths(x = ~long, y = ~lat, data = group_by(yk.df, group)) %>% 
+  add_legend('fill') %>%
+  add_legend('size', properties = legend_props(
+    legend = list(y = 50))) %>% 
+  set_options(duration = 0)
