aca6ec9ca44036966068119a3a81e3d007dd17ce
mhpob <mikeob9@gmail.com>
2015-03-31 13:41:54 -0400

    Plots can be made if range is less than 1.

--- active_exploratory.R
+++ active_exploratory.R
@@ -18,8 +18,15 @@ ld_mean <- listen.dat %>%
 
 # Plot general histogram, histogram where detections > 0, cumdist of detections
 hist_cum_plot <- function (var, width) {
-  brks <- seq(floor(range(ld_bot[, var])[1]), ceiling(range(ld_bot[, var])[2]),
-              width)
+  lims <- NULL
+  lims[1] <- ifelse(abs(min(ld_bot[, var])) < 1,
+                    min(ld_bot[, var]) - abs(min(ld_bot[, var])) / 5,
+                    floor(min(ld_bot[, var])))
+  lims[2] <- ifelse(abs(max(ld_bot[, var])) < 1,
+                    max(ld_bot[, var]) + abs(max(ld_bot[, var])) / 5,
+                    ceiling(max(ld_bot[, var])))
+  brks <- seq(lims[1], lims[2], width)
+  brks <- if(lims[2] > max(brks)) c(brks, max(brks) + width) else brks
   
   cum.dat <- substitute(ld_bot %>% filter(Detections > 0) %>% arrange(var) %>%
                            mutate(cumulative = cumsum(Detections),
@@ -31,8 +38,7 @@ hist_cum_plot <- function (var, width) {
   par(mar = c(5, 4, 4, 5) + 0.1)
   j <- hist(ld_bot[, var], breaks = brks, plot = F)
   hist(ld_bot[, var], breaks = brks,
-       xlim = c(floor(range(ld_bot[, var])[1]),
-                ceiling(range(ld_bot[, var])[2])),
+       xlim = lims,
        ylim = c(0, max(j$counts)),
        xlab = var,
        main = paste('Histogram of', var))
@@ -40,14 +46,12 @@ hist_cum_plot <- function (var, width) {
   par(new = T)
   hist(ld_bot[ld_bot$Detections > 0, var], breaks = brks,
        col = 'red', xaxt = "n", yaxt = "n", xlab = "", ylab = "", main = '',
-       xlim = c(floor(range(ld_bot[, var])[1]),
-                ceiling(range(ld_bot[, var])[2])),
+       xlim = lims,
        ylim = c(0, max(j$counts)))
   
   par(new = T)
   plot(cum.dat[, var], cum.dat[, 19], 
-       xlim = c(floor(range(ld_bot[, var])[1]),
-                ceiling(range(ld_bot[, var])[2])),
+       xlim = lims,
        type = 'l', col = 'blue',
        xaxt = "n", yaxt = "n", xlab = "", ylab = "", main = '',)
   axis(4, at = c(0, 20, 40, 60, 80, 100), col.axis = 'blue')
@@ -55,6 +59,7 @@ hist_cum_plot <- function (var, width) {
 }
 
 hist_cum_plot('Temp', 2)
+hist_cum_plot('pred.growth', 0.01)
 hist_cum_plot('DO.pct', 5)
 hist_cum_plot('DO.mg_l', 0.5)
 hist_cum_plot('Sal', 2)
@@ -62,21 +67,27 @@ hist_cum_plot('Cond', 2)
 hist_cum_plot('Depth', 2)
 
 # Plot histogram and overlay detections via points
-pts_histo <- function(var, brk){
+pts_histo <- function(var, width){
   ld_bot <- listen.dat %>%
     filter(Type == 'B')
+  lims <- NULL
+  lims[1] <- ifelse(abs(min(ld_bot[, var])) < 1,
+                    min(ld_bot[, var]) - abs(min(ld_bot[, var])) / 5,
+                    floor(min(ld_bot[, var])))
+  lims[2] <- ifelse(abs(max(ld_bot[, var])) < 1,
+                    max(ld_bot[, var]) + abs(max(ld_bot[, var])) / 5,
+                    ceiling(max(ld_bot[, var])))
+  brks <- seq(lims[1], lims[2], width)
+  brks <- if(lims[2] > max(brks)) c(brks, max(brks) + width) else brks
+  
+
   ld_bot <- ld_bot %>%  
-    mutate(bin = findInterval(ld_bot[, var],
-                              seq(floor(range(ld_bot[, var])[1]),
-                                  ceiling(range(ld_bot[, var])[2]), brk)))
+    mutate(bin = findInterval(ld_bot[, var], brks))
   det.bins <- ld_bot %>%
     group_by(bin) %>%
     summarize(detect = sum(Detections))
   
-  histo <- hist(ld_bot[, var], breaks = seq(floor(range(ld_bot[, var])[1]),
-                                            ceiling(range(ld_bot[, var])[2]),
-                                            brk),
-                plot = F)
+  histo <- hist(ld_bot[, var], breaks = brks, plot = F)
   mids <- data.frame(mids = histo$mids)
   mids$bin <- row.names(mids)
     
@@ -85,12 +96,10 @@ pts_histo <- function(var, brk){
 
   par(mar = c(5, 4, 4, 5) + 0.1)
   plot(histo, main = var, xlab = var, 
-       xlim = c(floor(range(ld_bot[, var])[1]),
-                ceiling(range(ld_bot[, var])[2])))
+       xlim = lims)
   par(new = T)
   plot(det.bins$mids, det.bins$detect,
-       xlim = c(floor(range(ld_bot[, var])[1]),
-                ceiling(range(ld_bot[, var])[2])),
+       xlim = lims,
        col = 'blue', xaxt = "n", yaxt = "n", xlab = "", ylab = "")
   axis(4, at = seq(0, ceiling(range(det.bins$detect)[2]), 5),
        col.axis = 'blue')
@@ -98,6 +107,7 @@ pts_histo <- function(var, brk){
 }
 
 pts_histo('Temp', 1)
+pts_histo('pred.growth', 0.01)
 pts_histo('DO.pct', 5)
 pts_histo('Sal', 2)
 pts_histo('Cond', 2)
