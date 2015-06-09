e2ca347469fe518b31b1b15b0a4c143d3dced81d
mhpob <mikeob9@gmail.com>
2015-06-09 11:31:14 -0400

    Add predicted growth.

--- dateVkm.R
+++ dateVkm.R
@@ -1,6 +1,9 @@
 library(dplyr); library(ggplot2)
 act.dat <- read.csv('p:/obrien/biotelemetry/csi/listening/activedata.csv',
                      stringsAsFactors = F)
+act.dat <- act.dat %>% 
+  mutate(pred.growth = TelemetryR::sturgrow(Temp, Sal, DO.pct),
+         river = substr(Site.ID, 1, 2))
 dist <- read.csv('distances.csv', header = F,
                  col.names = c('Site.ID', 'PKrivkm'))
 
@@ -54,15 +57,25 @@ dopct <-
   labs(x = 'Date (2014)', y = 'River Kilometer',
        size = 'Detections', color = 'D.O. (%)')
 
+predgr <-
+  ggplot() + geom_point(data = filter(pk.dat, Detections > 0, Type == 'B'),
+                        aes(x = Date, y = PKrivkm,
+                            size = factor(Detections), color = pred.growth)) +
+  scale_color_continuous(low = 'blue', high = 'orange') +
+  scale_size_manual(values = c(4,7,10,12), breaks = c('0','1','2','3')) +
+  ylim(c(0, 72)) +
+  labs(x = 'Date (2014)', y = 'River Kilometer',
+       size = 'Detections', color = 'Growth')
+
 library(gridExtra)
 png('p:/obrien/biotelemetry/csi/listening/2014 images/datevkm.png',
-    height = 900, width = 600)
-grid.arrange(sal, dopct, temp,
+    height = 650, width = 1200)
+grid.arrange(sal, dopct, temp, predgr,
              main = 'Pamunkey River Atlantic Sturgeon Detections')
 dev.off()
 
 ## River kilometer vs Depth vs Water Quality ###################################
-png('p:/obrien/biotelemetry/csi/listening/2014 images/kmvdepthvsal.png',
+png('p:/obrien/biotelemetry/csi/listening/2014 images/kmvdepthvpredgrowth.png',
     height = 775, width = 1200)
 ggplot() + geom_rect(data = pk.dat,
             aes(xmin = 1.5, xmax = 71.25,
@@ -77,10 +90,10 @@ ggplot() + geom_rect(data = pk.dat,
                 ymin = -0.25, ymax = 20),
             color = 'black', fill = NA) +
   geom_point(data = pk.dat,
-             aes(x = PKrivkm, y = Depth, color = Sal), size = 4) +
+             aes(x = PKrivkm, y = Depth, color = pred.growth), size = 4) +
   facet_wrap(~d.range, ncol = 1) +
   scale_x_reverse() + scale_y_reverse() +
   scale_color_continuous(low = 'blue', high = 'orange') +
-  labs(x = 'Pamunkey River Kilometer', y = 'Depth (m)', color = 'Salinity') +
+  labs(x = 'Pamunkey River Kilometer', y = 'Depth (m)', color = 'Growth') +
   theme_bw()
 dev.off()
