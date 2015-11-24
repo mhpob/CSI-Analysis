2c82b6204faf98f0c6e34f4032cd78e9b21ecd98
mhpob <mikeob9@gmail.com>
2015-11-24 17:34:36 -0500

    Update for second 2015 progress report.

--- prog_report_table.R
+++ prog_report_table.R
@@ -4,7 +4,7 @@ all.det <- vemsort('p:/obrien/biotelemetry/detections')
 ACT <- ACTtrans
 all <- data.frame(all.det)
 
-arr <- function(part){grepl(part, all[, 4], ignore.case = T)}
+arr <- function(part){grepl(part, all[, 8], ignore.case = T)}
 all$array <- ifelse(arr('cbl'), 'CBL Pier',
             ifelse(arr('cedar'), 'Cedar Point',
             ifelse(arr('piney'), 'Piney Point',
@@ -12,19 +12,22 @@ all$array <- ifelse(arr('cbl'), 'CBL Pier',
             ifelse(arr('kent'), 'Kent Island',
             ifelse(arr('chop'), 'Choptank',
             ifelse(arr('marsh'), 'Marshyhope',
-            ifelse(arr('nan'), 'Nanticoke',
+            ifelse(arr('nan|ferry|draw|broad|deep'), 'Nanticoke',
             ifelse(arr('poco'), 'Pocomoke',
-                    'Other')))))))))
+            ifelse(arr('v-|t-|a-'), 'MD Coast',
+                    'Other'))))))))))
 
 ACT$Common.Name <- ifelse(grepl("Striped bass", ACT$Common.Name, ignore.case = T),
                           "Striped bass", ACT$Common.Name)
 
 selected <- all %>%
-  filter(date.local >= '2014-06-03') %>%
+  filter(date.local >= '2015-07-01') %>%
   left_join(select(ACT, Tag.ID.Code.Standard, Common.Name, Primary.Researcher),
             by = c('transmitter' = 'Tag.ID.Code.Standard')) %>%
-  mutate(dates = lubridate::floor_date(date.local, unit = 'week')) %>%
-  group_by(Common.Name, array, Primary.Researcher, dates) %>%
   filter(Common.Name == 'Atlantic sturgeon') %>%
-  summarize(detections = n()) %>%
-  arrange(dates)
+  mutate(dates = lubridate::floor_date(date.local, unit = 'week')) %>%
+  group_by(Common.Name, dates, array, Primary.Researcher) %>%
+  summarize(detections = n())
+
+
+write.csv(selected, 'selected.csv')
